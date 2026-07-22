34804a8380c646d8ac4de3340269cc0c868bf3c6
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-22 11:40:16 +0200

    Called the properties in label element and in the input element and displayed them on the app tsx the main file

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,10 +1,15 @@
 import { Modal } from "../OverlayModal/Modal";
 import formStyle from "./Form.module.css";
+import { Input } from "../Input/Input";
 
 const Form = () => {
   return (
     <Modal>
-      <div className={formStyle["form-container"]}>{}</div>
+      <div className={formStyle["form-container"]}>
+        <Input label="Title" onChange={() => {}} />
+        <Input label="Url" onChange={() => {}} />
+        <Input label="description" onChange={() => {}} />
+      </div>
     </Modal>
   );
 };
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -11,6 +11,28 @@ type InputProps = {
   name?: string;
 };
 
-export const Input = () => {
-  return <div>Input</div>;
+export const Input: React.FC<InputProps> = ({
+  id,
+  value,
+  onChange,
+  style,
+  label,
+  error,
+  name,
+}) => {
+  return (
+    <div>
+      <label className={inputStyle["input-label"]}>{label}</label>
+      <input
+        name={name}
+        type="text"
+        id={id}
+        style={style}
+        value={value}
+        onChange={onChange}
+        className={inputStyle.input}
+      />
+      {error && <span className={inputStyle["input-error"]}>{error}</span>}
+    </div>
+  );
 };

bd0d947a257b6f58d6bf32ccf2fe8b8d6b5e8c1d
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-23 15:35:29 +0200

    added img logo and updated the container for it

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,11 +1,14 @@
 import { Modal } from "../OverlayModal/Modal";
 import mainStyle from "./Main.module.css";
+import logo from "./main asset/link-45deg.svg";
 
 const Main = () => {
   return (
     <div className={mainStyle["main-container"]}>
       <div className={mainStyle["logo-heading"]}>
-        <img src="" alt="" />
+        <div className={mainStyle["logo-container"]}>
+          <img src={logo} alt="link-img" height={20} width={20} />
+        </div>
       </div>
     </div>
   );

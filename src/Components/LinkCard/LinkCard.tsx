bf82979e7bc83084a2cf844cdfa9d80aa507c792
Samukelo Khathi <sbkhathi005@gmail.com>
2026-07-24 15:30:35 +0200

    Added basic styling for a card

--- src/Components/LinkCard/LinkCard.tsx
+++ src/Components/LinkCard/LinkCard.tsx
@@ -1,12 +1,19 @@
 import linkCardStyle from "./LinkCard.module.css";
 import { Text } from "../Text/Text";
+import Button from "../Button/Button";
 
 const LinkCard = () => {
   return (
     <div className={linkCardStyle["link-card"]}>
-      <Text variant={"p"} children={"Title"} />
-      <Text variant={"p"} children={"Url"} />
-      <Text variant={"p"} children={"Description"} />
+      <div className={linkCardStyle["text-container"]}>
+        <Text variant={"h1"} children={"Title"} />
+        <Text variant={"p"} children={"Url"} />
+        <Text variant={"p"} children={"Description"} />
+      </div>
+      <div className={linkCardStyle["btns-container"]}>
+        <Button text={"edit"} className={"edit-btn"} />
+        <Button text={"del"} className={"del-btn"} />
+      </div>
     </div>
   );
 };
